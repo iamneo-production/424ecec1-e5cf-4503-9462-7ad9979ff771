b93b9db9af69cbf8a9a02db136e896d440d443ee
sovickmajee <61087168+sovickmajee@users.noreply.github.com>
2022-04-08 07:48:16 +0530

    Update passengerlist.component.ts

--- angularapp/src/app/passengerlist/passengerlist.component.ts
+++ angularapp/src/app/passengerlist/passengerlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Passenger } from '../passenger';
+import { PassengerService } from '../passenger.service';
 
 @Component({
   selector: 'app-passengerlist',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PassengerlistComponent implements OnInit {
 
-  constructor() { }
+  _passengerlist:Passenger[];
+  constructor(private service:PassengerService,private route:Router, private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   
+    let id=parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.service.fetchPassengerListByUserIdFromRemote(id).subscribe(
+      data=>{
+          console.log("data recieved");
+          this._passengerlist=data;
+      },
+      error=>console.log("exception occured")
+
+    )
   }
-
+  goToEditPassenger(id:number){
+    this.route.navigate(['/editpassenger',id]);
+  }
+  deletePassenger(id:number){
+    this.service.deletePassengerByIdFromRemote(id).subscribe(
+      data=>{
+        console.debug("deleted successfully");
+        let uid=parseInt(localStorage.getItem('userid'));
+        this.route.navigate(['/passengerlist',uid]);
+      },
+      error=>{
+        console.log("Exception Occured");
+        let uid=parseInt(localStorage.getItem('userid'));
+        this.route.navigate(['/passengerlist',uid]);
+      }
+      
+    )
+  }
+  goToBook(id:number){}
+  goBack(){
+    this.route.navigate(['/addpassenger'])
+  }
 }
