1a30f3b2ef8660b1d235e70cb61daf1cb04fee05
KotiManeesha <koti18122.it@rmkec.ac.in>
2022-04-18 13:37:10 +0000

    more

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { BookingComponent } from './booking/booking.component';
 import { BookinglistComponent } from './bookinglist/bookinglist.component';
 import { UserbookinglistComponent } from './userbookinglist/userbookinglist.component';
 import { VehiclebookinglistComponent } from './vehiclebookinglist/vehiclebookinglist.component';
+import { AddvehicleComponent } from './addvehicle/addvehicle.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { VehiclebookinglistComponent } from './vehiclebookinglist/vehiclebooking
     BookingComponent,
     BookinglistComponent,
     UserbookinglistComponent,
-    VehiclebookinglistComponent
+    VehiclebookinglistComponent,
+    AddvehicleComponent,
+    AdmindashboardComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
