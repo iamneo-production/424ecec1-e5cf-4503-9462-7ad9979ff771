1bdb2b60a05a4cb26f4c246f90ed15c1c4b34cae
KotiManeesha <koti18122.it@rmkec.ac.in>
2022-04-18 13:46:39 +0000

    addon

--- angularapp/src/app/addvehicle/addvehicle.component.ts
+++ angularapp/src/app/addvehicle/addvehicle.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Vehicle } from '../vehicle';
+import { VehicleService } from '../vehicle.service';
 
 @Component({
   selector: 'app-addvehicle',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddvehicleComponent implements OnInit {
 
-  constructor() { }
+  vehicle=new Vehicle();
+
+  constructor(private route:Router,private service:VehicleService) { }
 
   ngOnInit(): void {
   }
 
+  addvehicleformsubmit()
+  {
+    this.service.saveVehicleToRemote(this.vehicle).subscribe(
+
+      data=>{
+              console.log("data added successfully");
+              this.route.navigate(['/getvehiclelist']);
+            },
+      error=>console.log("error")
+    )
+  }
+
+  goToVehiclelist(){
+    this.route.navigate(['/getvehiclelist']);
+  }
+  myimage:string="assets/images/1.jpg";
 }
