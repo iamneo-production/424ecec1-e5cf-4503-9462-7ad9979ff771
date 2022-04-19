9dda048bd06622b24b1d2c2e793e9d78d81d61d7
Siri257 <mahamkalisirisha57@gmail.com>
2022-04-19 14:33:16 +0000

    bookingbackend

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [
-  {path:'',redirectTo:'login',pathMatch:'full'},
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,40 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule }from '@angular/common/http';
-
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { UserdashboardComponent } from './userdashboard/userdashboard.component';
-import { AddvehicleComponent } from './addvehicle/addvehicle.component';
-import { EditvehicleComponent } from './editvehicle/editvehicle.component';
-import { VehiclelistComponent } from './vehiclelist/vehiclelist.component';
-import { ViewvehicleComponent } from './viewvehicle/viewvehicle.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    AdminDashboardComponent,
-    UserdashboardComponent,
-    AddvehicleComponent,
-    EditvehicleComponent,
-    VehiclelistComponent,
-    ViewvehicleComponent,
-  
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
